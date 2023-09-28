5d769d6b740207a6c907608dda99911c5545212b
nbena69 <naelbenaissapro@gmail.com>
2023-09-28 16:59:10 +0200

    RAS

--- app/Http/Controllers/FraisController.php
+++ app/Http/Controllers/FraisController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\MonException;
-use Request;
+use Illuminate\Http\Client\Request;
 use App\metier\Frais;
 use App\dao\ServiceFrais;
 use Exception;
@@ -40,6 +40,29 @@ class FraisController extends Controller
             $unFrais = $unServiceFrais->getById($id_frais);
             $titreVue = "Modification d'une fiche de frais";
             return view('vues/formFrais', compact('unFrais', 'titreVue', 'monErreur'));
+        } catch (MonException $e) {
+            $monErreur = $e->getMessage();
+            return view('vues/error', compact('monErreur'));
+        } catch (Exception $e) {
+            $monErreur = $e->getMessage();
+            return view('vues/error', compact('monErreur'));
+        }
+    }
+    public function validateFrais() {
+        try {
+            $id_frais = Request::input('id_frais');
+            $anneemois = Request::input('anneemois');
+            $nbjustificatifs = Request::input('nbjustificatifs');
+            $unServiceFrais = new ServiceFrais();
+            if ($id_frais > 0) {
+                $unServiceFrais->updateFrais($id_frais, $anneemois, $nbjustificatifs);
+            } else {
+                $montant = Request::input('montant');
+                $id_visiteur = Session::get('id');
+                $unServiceFrais->insertFrais($anneemois, $nbjustificatifs, $id_visiteur, $montant);
+            }
+
+            return redirect('/getListeFrais');
         } catch (MonException $e) {
             $monErreur = $e->getMessage();
             return view('vues/error', compact('monErreur'));
--- app/dao/ServiceFrais.php
+++ app/dao/ServiceFrais.php
@@ -7,14 +7,28 @@ use Illuminate\Support\Facades\DB;
 use App\Exceptions\MonException;
 use Illuminate\Support\Facades\Session;
 
-class ServiceFrais {
-    public function getFrais($id_visiteur) {
+class ServiceFrais
+{
+    public function getFrais($id_visiteur)
+    {
         try {
             $lesFrais = Db::table('frais')
                 ->Select()
                 ->where('frais.id_visiteur', '=', $id_visiteur)
                 ->get();
             return $lesFrais;
+        } catch (QueryException $e) {
+            throw new MonException($e->getMessage(), 5);
+        }
+    }
+
+    public function updateFrais($id_frais, $anneemois, $nbjustificatifs)
+    {
+        try {
+            $dateJour = date("Y-m-d");
+            DB::table('frais')
+                ->where('id_frais', '=', $id_frais)
+                ->update(['anneemois' => $anneemois, 'nbjustificatifs' => $nbjustificatifs, 'datemodification' => $dateJour]);
         } catch (QueryException $e) {
             throw new MonException($e->getMessage(), 5);
         }
