6db8d57ff26fb5539b149ca102fbf4a76d9e9815
nbena69 <naelbenaissapro@gmail.com>
2023-10-13 11:40:29 +0200

    Supression frais pas ok Lister Frais Hors Forfait ok

--- resources/views/vues/formFraisHors.blade.php
+++ resources/views/vues/formFraisHors.blade.php
@@ -32,23 +32,34 @@
                 <tr>
                     <td>{{$unFraisHors->lib_fraishorsforfait}}</td>
                     <td>{{$unFraisHors->montant_fraishorsforfait}}</td>
-                    <td style="text-align: center;"><a href="{{url('/modifierFraisHors')}}/{{$unFraisHors->id_fraishorsforfait}}">
+                    <td style="text-align: center;"><a
+                            href="{{url('/modifierFraisHors')}}/{{$unFraisHors->id_fraishorsforfait}}">
                         <span class="glyphicon glyphicon-pencil" data-toggle="tooltip" data-placement="top"
                               title="modification"></span>
                         </a></td>
                     <td style="text-align: center;"><a
                             class="glyphicon glyphicon-remove" data-toggle="tooltip" data-placement="top"
                             title="suppression" onclick="javascript:if (confirm('Suppression confirmée ?'))
-                        {window.location = '{{url('/supprimerFraisHors')}}/{{$unFraisHors->id_fraishorsforfait}}';}"></a></td>
+                        {window.location = '{{url('/supprimerFraisHors')}}/{{$unFraisHors->id_fraishorsforfait}}';}"></a>
+                    </td>
                 </tr>
             @endforeach
-        <td>Montant Total : </td>
-                <td></td>
+            <td>Montant Total :</td>
+            <td></td>
         @else
             <p>Aucun frais hors forfait trouvé.</p>
         @endif
     </table>
-
+    <div style="text-align: center">
+        <button class="btn btn-default btn-primary"
+                onclick="javascript: window.location = '{{url('/ajouter')}}';">
+            <span class="glyphicon glyphicon-plus"></span>Ajouter
+        </button>
+        <button type="submit" class="btn btn-default btn-primary"
+                onclick="javascript: window.location = '{{url('/valider')}}';">
+            <span class="glyphicon glyphicon-ok"></span>Valdier
+        </button>
+    </div>
 </div>
 <div class="col-md-6 col-md-offset-3">
     @if (!empty($erreur))
