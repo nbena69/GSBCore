ff536d634ce8329218b4cbe7d195414c45c09870
nbena69 <naelbenaissapro@gmail.com>
2024-04-30 12:37:01 +0200

    Filtrage visiteur fait, css fait

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Exceptions\MonException;
-use App\metier\Visiteur;
 use App\dao\ServiceVisiteur;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
-
-
 
 class VisiteurController extends Controller
 {
