ccb37c053b44ac6c0a0d085d154e44aecce53d07
nbena69 <naelbenaissapro@gmail.com>
2024-04-30 12:05:33 +0200

    Filtrage visiteur fait, css en cours

--- resources/views/vues/resultatsVisiteur.blade.php
+++ resources/views/vues/resultatsVisiteur.blade.php
@@ -5,12 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Résultats de la recherche</title>
 </head>
+@extends('layouts.master')
 <body>
-<h1>Résultats de la recherche</h1>
-<ul>
-    @foreach($visiteurs as $visiteur)
-        <li>{{ $visiteur->nom_visiteur }} - {{ $visiteur->id_laboratoire }}</li>
-    @endforeach
-</ul>
+<div class="container">
+    <h1 class="mt-5">Résultats de la recherche</h1>
+    <table class="table mt-4">
+        <thead>
+        <tr>
+            <th scope="col">Nom du Visiteur</th>
+            <th scope="col">Laboratoire</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($visiteurs as $visiteur)
+            <tr>
+                <td>{{ $visiteur->nom_visiteur }}</td>
+                <td>{{ $visiteur->laboratoire->nom_laboratoire }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
