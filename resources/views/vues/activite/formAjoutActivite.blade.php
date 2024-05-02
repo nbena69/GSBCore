6225e0fbfa3587a921ea2f88f08585a9a713f976
nbena69 <naelbenaissapro@gmail.com>
2024-05-02 16:40:04 +0200

    Activite En cours

--- resources/views/vues/activite/formAjoutActivite.blade.php
+++ resources/views/vues/activite/formAjoutActivite.blade.php
@@ -3,13 +3,13 @@
 
     {!! Form::open(['url' => 'validerActivite']) !!}
     <div class="col-md-12  col-sm-12 well well-md">
-        <center><h1></h1></center>
+        <center><h1>Ajouter une activité</h1></center>
         <div class="form-horizontal">
-            <input type="hidden" name="id_activite_compl" value=""/>
+            <input type="hidden" name="id_activite_compl" value="{{$uneActivite->id_activite_compl ?? 0}}"/>
             <div class="form-group">
                 <label class="col-md-3 col-sm-3 control-label">Date de l'activité : </label>
                 <div class="col-md-6 col-sm-6">
-                    <input type="text" name="date_activite" value="" class="form-control"
+                    <input type="text" name="date_activite" value="{{$uneActivite->date_activite ?? ''}}" class="form-control"
                            placeholder="Date de l'activité" required autofocus>
                 </div>
             </div>
@@ -17,21 +17,21 @@
             <div class="form-group">
                 <label class="col-md-3 col-sm-3 control-label">Lieu de l'activité : </label>
                 <div class="col-md-6  col-sm-6">
-                    <input type="text" name="lieu_activite" value=""
+                    <input type="text" name="lieu_activite" value="{{$uneActivite->lieu_activite ?? ''}}"
                            class="form-control" placeholder="Lieu de l'activité" required>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-md-3 col-sm-3 control-label">Thème de l'activité : </label>
                 <div class="col-md-6 col-sm-6">
-                    <input type="text" class="form-control" name="theme_activite" value=""
+                    <input type="text" class="form-control" name="theme_activite" value="{{$uneActivite->theme_activite ?? ''}}"
                            placeholder="Thème de l'activité" required>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-md-3 col-sm-3 control-label">Motif de l'activité : </label>
                 <div class="col-md-6 col-sm-6">
-                    <input type="text" class="form-control" name="motif_activite" value=""
+                    <input type="text" class="form-control" name="motif_activite" value="{{$uneActivite->motif_activite ?? ''}}"
                            placeholder="Motif de l'activité" required>
                 </div>
             </div>
